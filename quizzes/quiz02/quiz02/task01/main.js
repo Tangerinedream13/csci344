02c00f4ac0f1abf15ff6c83dcb2e2dd586f61ccf
Maria <mariabhaddon@gmail.com>
2024-12-12 12:54:36 -0500

    Step 1

--- quizzes/quiz02/quiz02/task01/main.js
+++ quizzes/quiz02/quiz02/task01/main.js
@@ -1,15 +1,14 @@
 // 1. Create your getBusinesses function here:
 
-
-
-
-
-
-
+async function getBusinesses(search_term, location, num_results) {
+    search_term.log
+    location.log
+    num_results.log
+}
 
 // 2. When you're done, uncomment the test code below and preview index.html in your browser:
 
-/*
+
 console.log(
     "Should display 3 pizza restaurants in Asheville:",
     getBusinesses("Asheville, NC", "pizza", 3)
@@ -18,4 +17,4 @@ console.log(
     "Should display 10 thai restaurants in San Francisco:",
     getBusinesses("San Francisco, CS", "thai", 10)
 );
-*/
+
